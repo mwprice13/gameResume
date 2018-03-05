5a46c1be5f11635d767a63ed220c02bc7ce5a8b2
mwprice13 <mwprice1113@gmail.com>
2018-03-04 20:46:26 -0500

    Update circle-bounds.js

--- src/engine/sprite/bounds/circle-bounds.js
+++ src/engine/sprite/bounds/circle-bounds.js
@@ -1,1 +1,22 @@
-﻿
+﻿class CircleBounds extends BoundsComponent {
+  constructor() {
+    super();
+    this.x = 0;
+    this.y = 0;
+    this.radius = 0;
+  }
+
+  setPosition(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  contains(x, y) {
+    return Math.hypot(this.x - x, this.y - y) <= this.radius;
+  }
+
+  isCollision(sprite) {
+    let dist = Math.hypot(this.x - sprite.bounds.x, this.y - sprite.bounds.y);
+    return dist <= this.radius + sprite.bounds.radius;
+  }
+}
