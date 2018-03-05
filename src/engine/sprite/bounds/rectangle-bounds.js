c7233a614dce3b1aaf9f2ce72643b27b9d241aef
mwprice13 <mwprice1113@gmail.com>
2018-03-04 20:46:05 -0500

    Update rectangle-bounds.js

--- src/engine/sprite/bounds/rectangle-bounds.js
+++ src/engine/sprite/bounds/rectangle-bounds.js
@@ -1,1 +1,31 @@
-﻿
+﻿class RectangleBounds extends BoundsComponent {
+  constructor() {
+    super();
+    this.x = 0;
+    this.y = 0;
+    this.width = 0;
+    this.height = 0;
+  }
+
+  setPosition(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  setSize(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  contains(x, y) {
+    return this.x <= x && x < this.x + this.width
+      && this.y <= y && y < this.y + this.height;
+  }
+
+  isCollision(sprite) {
+    return this.x < sprite.bounds.x + sprite.bounds.width
+      && this.x + this.width > sprite.bounds.x
+      && this.y < sprite.bounds.y + sprite.bounds.height
+      && this.y + this.height > sprite.bounds.y;
+  }
+}
